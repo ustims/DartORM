394d737b7a8c8d288d3179105e4efcf28a51ff61
Ustimenko Sergey <ustims@gmail.com>
2014-12-22 12:18:19 +0200

    mongodb tests refactoring

---
+++ test/mongodb/setup.js
@@ -0,0 +1,19 @@
+use dart_orm_test;
+
+if (db.version().toString().indexOf('2.5') > -1) {
+    db.addUser(
+        {
+            user: "dart_orm_test_user",
+            pwd: "dart_orm_test_user",
+            roles: ["readWrite"]
+        }
+    );
+} else {
+    db.createUser(
+        {
+            user: "dart_orm_test_user",
+            pwd: "dart_orm_test_user",
+            roles: [{role: "userAdmin", db: "dart_orm_test"}]
+        }
+    );
+}
--- test/mongodb_setup.sh
+++
@@ -1,1 +0,0 @@
-mongo test/mongodb_setup.js
