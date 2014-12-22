65924aa7771e4aff16bd0486b60e26fb9d59e469
Ustimenko Sergey <ustims@gmail.com>
2014-12-22 12:03:22 +0200

    mongodb fix

--- test/mongodb_setup.js
+++ test/mongodb_setup.js
@@ -1,14 +1,14 @@
 use dart_orm_test
 
-if (db.createUser) {
-    db.createUser(
-        {
-            user: "dart_orm_test_user",
-            pwd: "dart_orm_test_user",
-            roles: [{role: "userAdmin", db: "dart_orm_test"}]
-        }
-    )
-} else {
+//if (db.createUser) {
+//    db.createUser(
+//        {
+//            user: "dart_orm_test_user",
+//            pwd: "dart_orm_test_user",
+//            roles: [{role: "userAdmin", db: "dart_orm_test"}]
+//        }
+//    )
+//} else {
     db.addUser(
         {
             user: "dart_orm_test_user",
@@ -16,4 +16,4 @@ if (db.createUser) {
             roles: ["readWrite"]
         }
     )
-}
+//}
