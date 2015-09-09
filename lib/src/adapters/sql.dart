95c5c1b7979074b72f72f677fdb5d61a3af4267f
gardi <v.gordievskiy@gmail.com>
2015-09-09 13:15:22 +0000

    add Dart dynamic as json type

--- lib/src/adapters/sql.dart
+++ lib/src/adapters/sql.dart
@@ -372,6 +372,9 @@ class SQLAdapter {
       case 'LinkedHashMap':
         dbTypeName = 'json';
         break;
+      case 'dynamic':
+        dbTypeName = 'json';
+        break;
     }
     return dbTypeName;
   }
